dca68025d7acf6d9016b97f95454566fc6f480ef
hongshuhai <2922905784@qq.com>
2020-03-24 01:05:42 +0800

    登录实现一半

---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <cube-form :model="model" :schema="schema" @submit="submitHandler"></cube-form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      model: {
+        username: "",
+        password: ""
+      },
+      schema: {
+          fields: [
+            {
+              type: "input",
+              modelKey: "username",
+              label: "用户名",
+              props: {
+                placeholder: "请输入用户名"
+              },
+              rule: {
+                // 校验规则
+                required: true,
+                type: "string",
+                min: 3,
+                max: 15
+              },
+              trigger: "blur",
+              messages: {
+                required: "用户名不能为空",
+                min: "用户名不能少于3个字符",
+                max: "用户名不能大于15个字符"
+              }
+            }
+          ]
+        }
+    };
+  }
+  ,
+  methods:{
+      submitHandler(){},
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
